34cde3208832b87d107f82cbd72e0723f245242b
Malika Oubilla <malikaoubilla@gmail.com>
2023-11-20 02:04:33 -0800

    task 0.2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,7 +3,7 @@
 /**
  * bubble_sort - Sort an array of integers in ascending order
  * @array: An array of integers to sort
- * @size: The size of the array
+ * @n: The size of the array
  */
 void bubble_sort(int *array, size_t n)
 {
@@ -24,7 +24,7 @@ void bubble_sort(int *array, size_t n)
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
 				swap = 1;
-				print_array(array, size);
+				print_array(array, n);
 			}
 		}
 		size--;
